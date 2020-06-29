5942a0a0cbb88202741eb3107c84ac23b021161a
BelyaevaEkaterina <56511688+BelyaevaEkaterina@users.noreply.github.com>
2020-06-29 18:31:19 +0400

    Исправления по замечаниям

--- js/main.js
+++ js/main.js
@@ -9,13 +9,10 @@ var MIN_ROOMS = 1;
 var MAX_ROOMS = 100;
 var MIN_GUESTS = 1;
 var MAX_GUESTS = 10;
-var PIN_WIDTH = 62;
-var PIN_HEIGHT = 84;
-var MAP = document.querySelector('.map');
-var FRAGMENT = document.createDocumentFragment();
-var PIN_TEMPLATE = document.querySelector('#pin')
-  .content
-  .querySelector('.map__pin');
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
+var GAP_X = Math.round(PIN_WIDTH / 2);
+
 var TYPE_OF_HOUSE = ['palace', 'flat', 'house', 'bungalo'];
 var CHECK_TIME = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -23,59 +20,68 @@ var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-var renderRandomValue = function (min, max) {
+var map = document.querySelector('.map');
+var fragment = document.createDocumentFragment();
+var pinTemplate = document.querySelector('#pin')
+    .content
+    .querySelector('.map__pin');
+
+var generateRandomNumber = function (min, max) {
   return Math.round(min + Math.random() * (max - min));
 };
 
-var renderElement = function (arr) {
+var getRandomElementOfArray = function (arr) {
   var lastIndex = arr.length - 1;
   var i = Math.round(Math.random() * lastIndex);
 
   return arr[i];
 };
 
-var renderSomeElements = function (arr) {
+var getSomeElementsFromArray = function (arr) {
   var newArr = [];
-  var num = renderRandomValue(1, arr.length - 1);
+  var tempArray = arr.slice();
+  var num = generateRandomNumber(1, arr.length - 1);
   for (var i = 0; i < num; i++) {
-    newArr[i] = renderElement(arr);
+    var randomNum = generateRandomNumber(0, tempArray.length-1);
+    newArr[i] = tempArray[randomNum];
+    tempArray.splice(randomNum, 1);
   }
   return newArr;
 };
 
-var renderAd = function (num) {
-  var arr = [];
+var generateAds = function (num) {
+  var ads = [];
   for (var i = 0; i < num; i++) {
-    var arrElement = {
+    var ad = {
       author: {
         avatar: 'img/avatars/user0' + String(i + 1) + '.png'
       },
       offer: {
         title: 'Заголовок',
-        address: String(renderRandomValue(0, MAP.offsetWidth)) + ', ' + String(renderRandomValue(MIN_Y, MAX_Y)),
-        price: renderRandomValue(MIN_PRICE, MAX_PRICE),
-        type: renderElement(TYPE_OF_HOUSE),
-        rooms: renderRandomValue(MIN_ROOMS, MAX_ROOMS),
-        guests: renderRandomValue(MIN_GUESTS, MAX_GUESTS),
-        checkin: renderElement(CHECK_TIME),
-        checkout: renderElement(CHECK_TIME),
-        features: renderSomeElements(FEATURES),
+        address: String(generateRandomNumber(0, map.offsetWidth)) + ', ' + String(generateRandomNumber(MIN_Y, MAX_Y)),
+        price: generateRandomNumber(MIN_PRICE, MAX_PRICE),
+        type: getRandomElementOfArray(TYPE_OF_HOUSE),
+        rooms: generateRandomNumber(MIN_ROOMS, MAX_ROOMS),
+        guests: generateRandomNumber(MIN_GUESTS, MAX_GUESTS),
+        checkin: getRandomElementOfArray(CHECK_TIME),
+        checkout: getRandomElementOfArray(CHECK_TIME),
+        features: getSomeElementsFromArray(FEATURES),
         description: 'Описание',
-        photos: PHOTOS
+        photos: getSomeElementsFromArray(PHOTOS)
       },
       location: {
-        x: renderRandomValue(0, MAP.offsetWidth),
-        y: renderRandomValue(MIN_Y, MAX_Y)
+        x: generateRandomNumber(GAP_X, map.offsetWidth - GAP_X),
+        y: generateRandomNumber(MIN_Y, MAX_Y)
       }
     };
-    arr[i] = arrElement;
+    ads[i] = ad;
   }
 
-  return arr;
+  return ads;
 };
 
 var renderPin = function (ad) {
-  var pin = PIN_TEMPLATE.cloneNode(true);
+  var pin = pinTemplate.cloneNode(true);
   var pinImage = pin.querySelector('img');
 
   pin.style = 'left: ' + String(ad.location.x - PIN_WIDTH / 2) + 'px; top: ' +
@@ -86,10 +92,10 @@ var renderPin = function (ad) {
   return pin;
 };
 
-MAP.classList.remove('map--faded');
+map.classList.remove('map--faded');
 
-var arrAd = renderAd(AMOUNT_AD);
+var advertisements = generateAds(AMOUNT_AD);
 for (var i = 0; i < AMOUNT_AD; i++) {
-  FRAGMENT.appendChild(renderPin(arrAd[i]));
+  fragment.appendChild(renderPin(advertisements[i]));
 }
-document.querySelector('.map__pins').appendChild(FRAGMENT);
+document.querySelector('.map__pins').appendChild(fragment);
